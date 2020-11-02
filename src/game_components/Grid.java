f19ae8ca7ac0f8d5432ba4fd97ad6ad05f6d0d87
Valentin Escobosa <dovakhan.escobosa@gmail.com>
2020-11-02 12:19:22 +0100

    Le win marche pas

--- src/game_components/Grid.java
+++ src/game_components/Grid.java
@@ -20,8 +20,24 @@ public class Grid {
         }
     }
 
-    public boolean win(int x, int y){
-        if(this.length)
+    public int linearWin(int x, int y, int stepX, int stepY,char pion){
+        int count = 0;
+        for(int i=0;i<4;i++){
+            if(x+i*stepX<0 || x+i*stepX > this.width || y+i*stepY<0 || y+i*stepY > this.length){
+                return count;
+            }
+            if(this.grid[x+i*stepX][y+i*stepY] != pion){
+                return count;
+            }
+            count++;
+        }
+        return count;
+    }
+
+    public boolean win(int x, int y, char pion){
+        int count = 0;
+        count = linearWin(x,y,1,0,pion) + linearWin(x,y,-1,0,pion);
+        return count>3;
     }
 
     public void turn(int column,char pion){
